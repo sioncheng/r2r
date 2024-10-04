331c9acbc4d1e635f9704656ac665ea8f807410d
程勇桥 <cyq@mini18.local>
2024-10-04 08:25:09 +0800

    searching from remote api

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -1,6 +1,5 @@
-import { useState , useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState , useEffect, useRef, useReducer, useCallback } from 'react'
+import axios from 'axios'
 import './App.css'
 
 
@@ -8,6 +7,8 @@ function List(props) {
 
   function Item(props) {
     const e = props.item;
+    const onRemoveItem = props.onRemoveItem;
+
     return (
       <li>
         <span>
@@ -16,6 +17,9 @@ function List(props) {
         <span>{e.author}</span>
         <span>{e.num_comments}</span>
         <span>{e.points}</span>
+        <span>
+          <button type='button' onClick={() => onRemoveItem(e)}>dismiss</button>
+        </span>
       </li>
     );
   }
@@ -24,23 +28,41 @@ function List(props) {
     <ul>
       {/* comments */}
       {
-        props.stories.map(e => (<Item key={e.objectID} item={e}/>))
+        props.stories.map(e => (<Item key={e.objectID} item={e} onRemoveItem={props.onRemoveItem}/>))
       }
     </ul>
   );
 }
 
-function Search ({searchTerm, onSearch}) {
-  //const {searchTerm, onSearch} = props   
+function InputWithLabel ({id, label, value, onInputChange, isFocused, children}) {
+  const inputRef = useRef();
+  useEffect(() => {
+    if (isFocused && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [isFocused]);
+
   return (
     <>
-      <label htmlFor='search'>Search:</label>
-      <input type='text' id='search' value={searchTerm} onChange={onSearch}/>
-      <p>
-        Searching for <strong>{searchTerm}</strong>
-      </p>
+      <label htmlFor={id}>{children}</label>
+      &nbsp;
+      <input type='text' id={id} value={value} onChange={onInputChange} ref={inputRef}/>
+    </>
+  );
+}
 
-    </> /* react fragment */
+function SearchForm({onSubmit, searchTerm, onSearch}) {
+  return (
+    <form onSubmit={onSubmit}>
+        <InputWithLabel id='search' 
+          value={searchTerm} 
+          onInputChange={onSearch}
+          isFocused={true}>
+          Search:
+        </InputWithLabel>
+        &nbsp;&nbsp;
+        <button type='submit' disabled={!searchTerm}>Submit</button>
+    </form>
   );
 }
 
@@ -48,7 +70,7 @@ function Search ({searchTerm, onSearch}) {
 function App() {
   
 
-  const list = [
+  const initStories = [
     {
       title: 'React',
       url: 'https://reactjs.rog',
@@ -67,11 +89,56 @@ function App() {
     }
   ];
 
+  const [STORY_ACTION_LDG, STORY_ACTION_SET, STORY_ACTION_DEL, STORY_ACTION_ERR] = ['LDG', 'SET', 'DEL', 'ERR'];
+
+  const storyReducer = (state, action) => {
+    switch (action.type) {
+      case STORY_ACTION_LDG:
+        return {...state, isLoading: true}
+      case STORY_ACTION_SET:
+        return {...state, isLoading: false, data: action.payload}
+      case STORY_ACTION_DEL:
+        const newData = state.data.filter((s) => s.objectID != action.payload.objectID);
+        return {...state, data: newData}; 
+        case STORY_ACTION_ERR:
+          return {...state, isLoading: false, isError: true}
+      default:
+        throw new Error("unknow [" + action.type + "] for dispatch stories");
+    }
+  };
+
+
+  const [searchTerm, setSearchTerm] = useStorageState('search', 'React');
+  const [url, setUrl] = useState('')
+  const [story, storyDispatcher] = useReducer(storyReducer, 
+    {data:[], isLoading:false, isError: false});
+
+  const fetchStory = useCallback(async () => {
+    if (!url) {
+      return;
+    }
+    storyDispatcher({type:STORY_ACTION_LDG, payload:true});
+    console.log("searching " + url , new Date());
+    try {
+      const result = await axios.get(url);
+      storyDispatcher({type:STORY_ACTION_SET, payload: result.data.hits});
+    } catch (e) {
+      console.log("fetchStory failed", e);
+      storyDispatcher({type:STORY_ACTION_ERR});
+    }
+  }, [url]);
+
+  useEffect(() => {
+    fetchStory();
+  }, [fetchStory]);
+
+  function handleRemoveItem(item) {
+    storyDispatcher({type:STORY_ACTION_DEL, payload: item});
+  }
+
 
   function useStorageState(key, initState) {
-
     const lv = localStorage.getItem(key);
-
     const [value, setValue] = useState(null == lv ? initState : lv);
 
     useEffect(() => { 
@@ -85,22 +152,35 @@ function App() {
     return [value, setValue];
   }
 
-  const [searchTerm, setSearchTerm] = useStorageState('search', 'React');
-
   function onSearch(e) {
     //console.log('onSearch', e.target.value);
     setSearchTerm(e.target.value);
   }
 
-  const searchedStories = list.filter( e => e.title.includes(searchTerm));
+  function onSearchSubmit(e) {
+    e.preventDefault();
+    if (!searchTerm) {
+      return;
+    }
+    setUrl('https://hn.algolia.com/api/v1/search?query=' + searchTerm);
+  }
+
+  const searchedStories = story.data.filter( e => e.title.includes(searchTerm));
 
   return (
     <>
       <div>
         <h1>Hacker Stories</h1>
-        <Search onSearch={onSearch} searchTerm={searchTerm}/>
+        <SearchForm searchTerm={searchTerm} onSearch={onSearch} onSubmit={onSearchSubmit}></SearchForm>
         <hr/>
-        <List stories={searchedStories}/>
+        {story.isError && (<p>Something went wrong...</p>)}
+        {story.isLoading ?
+          (<p>loading...</p>)
+          :
+          (
+            <List stories={searchedStories} onRemoveItem={handleRemoveItem}/>
+          )
+        }
       </div>
     </>
   );
